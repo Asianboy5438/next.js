92fde981ef7f959711d255b52d36fb93b2d54fb5
Oscar Lieu <oscarlieu@gmail.com>
2026-05-03 17:56:23 -0700

    Lab 17: Updated prisma schema and implemented template code

--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,68 @@
 import styles from "./page.module.css";
-import Card from "../components/Card";
-import Filter from "../components/Filters";
 import Link from "next/link";
+import Filters from "@/components/Filters";
+import prisma from '@/app/lib/prisma'
+
+export const runtime = "nodejs";
 
 async function fetchTitles() {
-  const response = await fetch(
-    "https://web.ics.purdue.edu/~zong6/profile-app/get-titles.php",
-    {
-      next: { revalidate: 60 },
-    },
-  );
-  const titles = await response.json();
-  return titles ? titles.titles : [];
+  const data = await prisma.profiles.findMany({
+    distinct: ["title"],
+    select: { title: true },
+  });
+  return data ? data.map((t) => t.title) : [];
 }
+
 async function getData({ title, search }) {
-  const response = await fetch(
-    `https://web.ics.purdue.edu/~zong6/profile-app/fetch-data-with-filter.php?title=${title}&name=${search}&limit=1000`,
-    {
-      next: { revalidate: 60 },
+  const profiles = await prisma.profiles.findMany({
+    where: {
+      ...(title && { title: { contains: title, mode: "insensitive" } }),
+      ...(search && { name: { contains: search, mode: "insensitive" } }), // ✅ TODO fixed
     },
-  );
-  const data = await response.json();
-  return data ? data.profiles : [];
+  });
+  return profiles;
 }
-export const metaData = {
-  title: "Profile App",
-  description: "A profile app built with Next.js",
-};
+
 export default async function Home({ searchParams }) {
   const searchParamsData = await searchParams;
-  const selectedTitle = searchParamsData && searchParamsData.title ? searchParamsData.title : "";
-  const selectedSearch = searchParamsData && searchParamsData.search ? searchParamsData.search : "";
+  const selectedTitle = searchParamsData?.title || "";
+  const search = searchParamsData?.search || "";
   const [titles, profiles] = await Promise.all([
     fetchTitles(),
-    getData({ title: selectedTitle, search: selectedSearch }),
+    getData({ title: selectedTitle, search }),
   ]);
-  console.log(titles);
 
   return (
-    <div>
-        <h1>My Profiles App</h1>
-        <Filter titles={titles} title={selectedTitle} search={selectedSearch} />
-        <div className="grid">
-        {profiles && profiles.length > 0
-          ? profiles.map((profile) => (
-            <Link key={profile.id} href={`/profile/${profile.id}`}> 
-              <Card
-                name={profile.name}
-                title={profile.title}
-                image={profile.image_url}
-              />
-              </Link>
-            ))
-          : <p>No profiles found.</p>}
-          </div>
-    </div>
+    <main className={styles.main}>
+      <div className="section">
+        <div className="container">
+          <h1>Profile App </h1>
+          <Filters titles={titles} title={selectedTitle} search={search} />
+          {profiles.length === 0 ? (
+            <p>No profiles found.</p>
+          ) : (
+            <div className="grid">
+              {profiles.map((profile) => (
+                <Link key={profile.id} href={`/profile/${profile.id}`}>
+                  <div className={styles["profile-card"]}>
+                    <div className={styles["profile-card__image"]}>
+                      <img
+                        src={profile.image_url || "/vercel.svg"}
+                        alt={profile.name}
+                      />
+                    </div>
+                    <div className={styles["profile-card__content"]}>
+                      <p>{profile.name}</p>
+                      <p>{profile.title}</p>
+                      <p>{profile.email}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
   );
-}
+} 
